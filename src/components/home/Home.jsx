636d44f51ad23db204f1f186cb434ffe736ac8a1
Lisa Ferrier <lisaferrrier@gmail.com>
2018-10-30 14:47:59 +0100

    add components files + home files + add container fluid

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./Home.css";
+import { Container } from 'reactstrap';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Container fluid>
+      </Container>
+    )
+  }
+}
